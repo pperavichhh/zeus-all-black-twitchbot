da6925422f28e8efc52e8809fbf6d42c4b56e9af
U Peravich <85332850+pperavichhh@users.noreply.github.com>
2022-01-24 15:52:16 +0700

    Delete constants.js

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const CHANNEL_NAME = "zeusallblack";
-export const OAUTH_TOKEN = 'aywy1l8gvvgpv2gzzyir3m04otl88s';
-export const BOT_USERNAME = 'zeusallblackbot'
-// export const BLOCKED_WORDS = []
