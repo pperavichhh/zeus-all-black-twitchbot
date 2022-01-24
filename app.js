721ce7b824c639b2b572aa1b00bc11c244750adf
U Peravich <85332850+pperavichhh@users.noreply.github.com>
2022-01-24 15:25:26 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+import tmi from 'tmi.js'
+import { BOT_USERNAME , OAUTH_TOKEN, CHANNEL_NAME } from './constant'
+
+const options = {
+	options: { debug: true },
+	connection: {
+    reconnect: true,
+    secure: true,
+    timeout: 180000,
+    reconnectDecay: 1.4,
+    reconnectInterval: 1000,
+	},
+	identity: {
+		username: BOT_USERNAME,
+		password: OAUTH_TOKEN
+	},
+	channels: [ CHANNEL_NAME ]
+}
+
+const client = new tmi.Client(options)
+
+client.connect()
+
+// events
+
+
+client.on('hosted', (channel, username, viewers, autohost) => {
+  onHostedHandler(channel, username, viewers, autohost)
+})
+
+client.on('subscription', (channel, username, method, message, userstate) => {
+  onSubscriptionHandler(channel, username, method, message, userstate)
+})
+
+client.on('raided', (channel, username, viewers) => {
+  onRaidedHandler(channel, username, viewers)
+})
+
+client.on('cheer', (channel, userstate, message) => {
+  onCheerHandler(channel, userstate, message)
+})
+
+client.on('giftpaidupgrade', (channel, username, sender, userstate) => {
+  onGiftPaidUpgradeHandler(channel, username, sender, userstate)
+})
+
+client.on('hosting', (channel, target, viewers) => {
+  onHostingHandler(channel, target, viewers)
+})
+
+client.on('resub', (channel, username, months, message, userstate, methods) => {
+  resubHandler(channel, username, months, message, userstate, methods)
+})
+
+client.on('subgift', (channel, username, streakMonths, recipient, methods, userstate) => {
+  subGiftHandler(channel, username, streakMonths, recipient, methods, userstate)
+})
+
+// event handlers
+
+client.on('message', (channel, userstate, message, self) => {
+  if(self) {
+    return
+  }
+})
+
+// function onMessageHandler (channel, userstate, message, self) {
+//   checkTwitchChat(userstate, message, channel)
+// }
+
+
+function onHostedHandler (channel, username, viewers, autohost) {
+  client.say(channel,
+    `ขอบคุณ คุณ @${username} สำหรับการ Host ด้วยคนดู ${viewers} คนด้วยค้าบ`
+  )
+}
+
+function onRaidedHandler(channel, username, viewers) {
+  client.say(channel,
+    `ขอบคุณ คุณ @${username} สำหรับการ raid ด้วยคนดู ${viewers} คน ขอบคุณที่ raid มาดูผมโง่นะครับ`
+  )
+}
+
+function onSubscriptionHandler(channel, username, method, message, userstate) {
+  client.say(channel,
+    `ขอบคุณ คุณ @${username} สำหรับการติดตามด้วยนะค้าบ`
+  )
+}
+
+function onCheerHandler(channel, userstate, message)  {
+  client.say(channel,
+    `ขอบคุณ คุณ @${userstate.username} สำหรับ ${userstate.bits} บิท ด้วยค้าบ แต่ทีหลังขอเยอะกว่านี้นะ ตังค์กินข้าวไม่มี `
+  )
+}
+
+function onGiftPaidUpgradeHandler(channel, username, sender, userstate) {
+  client.say(channel,
+    `ขอบคุณ คุณ @${username} สำหรับการต่อซํบที่ได้มาจาก gift sub ด้วยค้าบ`
+  )
+}
+
+function onHostingHandler(channel, target, viewers) {
+  client.say(channel,
+    `เทำเจ้าแบงค์กำลัง Hosting ${target} ด้วยคนดู ${viewers} คน`
+  )
+}
+
+
+function resubHandler(channel, username, months, message, userstate, methods) {
+  const cumulativeMonths = userstate['msg-param-cumulative-months']
+  client.say(channel,
+    `ขอบคุณ คุณ @${username} สำหรับซํบ ${cumulativeMonths}เดือน ด้วยค้าบ`
+  )
+}
+
+function subGiftHandler(channel, username, streakMonths, recipient, methods, userstate) {
+
+  client.say(channel,
+    `ขอบคุณ คุณ @${username} ที่ giftsub ให้ คุณ ${recipient} ด้วยค้าบ`
+  )
+
+  // this comes back as a boolean from twitch, disabling for now
+  // "msg-param-sender-count": false
+  // const senderCount =  ~~userstate["msg-param-sender-count"];
+  // client.say(channel,
+  //   `${username} has gifted ${senderCount} subs!`
+  // )
+}
+
+// commands
+
+
+// function checkTwitchChat(userstate, message, channel) {
+//   console.log(message)
+//   message = message.toLowerCase()
+//   let shouldSendMessage = false
+//   shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
+//   if (shouldSendMessage) {
+//     // tell user
+//     client.say(channel, `@${userstate.username}, เบาน้องเบา`)
+//     // delete message
+//     client.deletemessage(channel, userstate.id)
+//   }
+// }
---
+++ constant.js
@@ -0,0 +1,4 @@
+export const CHANNEL_NAME = "zeusallblack";
+export const OAUTH_TOKEN = 'aywy1l8gvvgpv2gzzyir3m04otl88s';
+export const BOT_USERNAME = 'zeusallblackbot'
+// export const BLOCKED_WORDS = []
